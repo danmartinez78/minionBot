d18cea2c3dee5f45b3a48524a1147d5fa7acefa7
Daniel Martinez <danmartinez78@gmail.com>
2020-10-10 23:07:35 -0500

    added twitch bot functionality and tested uarm interface

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os # for importing env vars for the bot to use
+from twitchio.ext import commands
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='.env')
+
+bot = commands.Bot(
+    # set up the bot
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+@bot.event
+async def event_message(ctx):
+    'Runs every time a message is sent in chat.'
+
+    # make sure the bot ignores itself and the streamer
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+        return
+
+    await ctx.channel.send(ctx.content)
+
+
+
+if __name__ == "__main__":
+    bot.run()
