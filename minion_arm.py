40e0e896494baa6620677f71eb64291f141c07a1
Daniel Martinez <danmartinez78@gmail.com>
2020-10-11 08:20:35 -0500

    first pass done and deployed

---
+++ minion_arm.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import time
+import functools
+sys.path.append(os.path.join(os.path.dirname(__file__), './uArm-Python-SDK/'))
+from uarm.wrapper import SwiftAPI
+
+class MinionArm:
+    def __init__(self):
+        self.swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'})
+        self.swift.waiting_ready()
+        device_info = self.swift.get_device_info()
+        print(device_info)
+        firmware_version = device_info['firmware_version']
+        if firmware_version and not firmware_version.startswith(('0.', '1.', '2.', '3.')):
+             self.swift.set_speed_factor(0.0005)
+        self.speed = 110000
+        self.test()
+        self.board = "right"
+
+    def test(self):
+        # self.swift.set_buzzer(frequency=1000, duration=0.5, wait=True)
+        self.swift.reset(wait=True, speed=self.speed)
+        self.move(1250, 0, 150, self.speed)
+        print(self.swift.get_position())
+        # self.swift.set_buzzer(frequency=1000, duration=0.5, wait=True)
+
+    def home(self):
+        print("going home")
+        self.swift.set_position(x = 150 , speed = self.speed * 0.5, wait=True)
+        time.sleep(2)
+        self.swift.set_position(x = 125 , y = 0, speed = self.speed * 0.5, wait=True)
+        self.move(125, 0, 150, self.speed)
+        time.sleep(2)
+        # self.swift.set_buzzer(frequency=1000, duration=0.5, wait=True)
+
+
+    def move(self, x, y, z, speed):
+        self.swift.set_position(x, y, z, speed, wait=True)
+
+    def flip(self):
+        print("flipping")
+        # self.home()
+        self.swift.set_buzzer(frequency=1000, duration=0.5, wait=True)
+        if self.board == "right":
+            self.flip_left()
+            self.board = "left"
+        else:
+            self.flip_right()
+            self.board = "right"
+        self.home()
+        self.swift.set_buzzer(frequency=500, duration=1.0, wait=True)
+
+    def flip_left(self):
+        self.move(180, 0, 150, self.speed)
+        self.move(220, 0, 150, self.speed)
+        self.move(220, 10, 140, self.speed*.5)
+        self.move(220, 30, 130, self.speed*.5)
+        self.move(220, 40, 110, self.speed*.5)
+        self.move(220, 60, 100, self.speed*.5)
+        self.move(220, 60, 30, self.speed*.5)
+        time.sleep(2)
+
+    def flip_right(self):
+        self.move(180, 0, 150, self.speed)
+        self.move(220, -10, 150, self.speed)
+        self.move(220, -20, 140, self.speed*.8)
+        self.move(220, -40, 130, self.speed*.8)
+        self.move(220, -50, 110, self.speed*.8)
+        self.move(220, -60, 100, self.speed*.8)
+        self.move(220, -60, 30, self.speed*.8)
+        time.sleep(2)
+    
+    def disconnect(self):
+        self.swift.disconnect()  
--- minion_bot.py
+++ minion_bot.py
@@ -2,6 +2,11 @@ import os # for importing env vars for the bot to use
 from twitchio.ext import commands
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env')
+from minion_arm import MinionArm
+import time
+
+
+myarm = MinionArm()
 
 bot = commands.Bot(
     # set up the bot
@@ -12,11 +17,17 @@ bot = commands.Bot(
     initial_channels=[os.environ['CHANNEL']]
 )
 
+myarm.flip()
+myarm.flip()
+
+
+
 @bot.event
 async def event_ready():
     'Called once when the bot goes online.'
     print(f"{os.environ['BOT_NICK']} is online!")
     ws = bot._ws  # this is only needed to send messages within event_ready
+    myarm.home()
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
 
 @bot.event
@@ -27,7 +38,23 @@ async def event_message(ctx):
     if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
         return
 
-    await ctx.channel.send(ctx.content)
+    await bot.handle_commands(ctx)
+    
+    if 'hello' in ctx.content.lower():
+        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+
+    
+
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.send('test passed!')
+
+@bot.command(name='flip')
+async def flip(ctx):
+    await ctx.send('Flipping, please wait...')
+    myarm.flip()
+    time.sleep(2)
+    await ctx.send('Done!')
 
 
 
