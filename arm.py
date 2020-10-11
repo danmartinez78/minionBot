dcc756ff6d1fff978edf810f1b30f94ae4a31f35
Daniel Martinez <danmartinez78@gmail.com>
2020-10-10 22:19:13 -0500

    added uarm python sdk as git submodule and started arm class

---
+++ arm.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import time
+import functools
+sys.path.append(os.path.join(os.path.dirname(__file__), './uArm-Python-SDK/'))
+from uarm.wrapper import SwiftAPI
+
+class uarm:
+    def __init__(self):
+        pass
+        
+    def home(self):
+        NotImplemented
+
+    def move_to_pos(x, y, z, speed):
+        pass
