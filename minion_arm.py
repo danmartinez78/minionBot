d93e9f9b9941b8480f3228bd4ad7c961faf0a746
Daniel Martinez <danmartinez78@gmail.com>
2020-10-13 11:29:42 -0500

    small changes

--- minion_arm.py
+++ minion_arm.py
@@ -27,15 +27,13 @@ class MinionArm:
 
     def home(self):
         print("going home")
-        self.swift.set_position(x = 150 , speed = self.speed * 0.5, wait=True)
-        time.sleep(2)
-        self.swift.set_position(x = 125 , y = 0, speed = self.speed * 0.5, wait=True)
-        self.move(125, 0, 150, self.speed)
+        self.swift.set_position(x = 150 , y = 0, speed = self.speed * 0.5, wait=True)
+        self.move(145, 0, 100, self.speed*1.5)
         time.sleep(2)
         # self.swift.set_buzzer(frequency=1000, duration=0.5, wait=True)
 
-    def downward_home(self):
-        print("going home")
+    def rest(self):
+        print("going to rest position")
         self.swift.set_position(x = 150 , speed = self.speed * 0.5, wait=True)
         self.swift.set_position(x = 125 , y = 0, speed = self.speed * 0.5, wait=True)
         self.move(125, 0, 40, self.speed * 0.5)
@@ -61,23 +59,32 @@ class MinionArm:
 
     def flip_left(self):
         self.move(180, 0, 150, self.speed)
-        self.move(220, 0, 150, self.speed)
-        self.move(220, 10, 140, self.speed*.5)
-        self.move(220, 30, 130, self.speed*.5)
-        self.move(220, 40, 110, self.speed*.5)
-        self.move(220, 60, 100, self.speed*.5)
-        self.move(220, 60, 30, self.speed*.5)
+        self.move(240, 0, 150, self.speed)
+        # self.move(220, 10, 140, self.speed*.2)
+        self.move(220, 40, 150, self.speed*.2)
+        # self.move(220, 40, 110, self.speed*.2)
+        # self.move(220, 60, 100, self.speed*.2)
+        self.move(220, 50, 30, self.speed*.2)
+        time.sleep(1)
+        self.move(180, 50, 20, self.speed*.2)
         time.sleep(2)
 
     def flip_right(self):
         self.move(180, 0, 150, self.speed)
-        self.move(220, -10, 150, self.speed)
-        self.move(220, -20, 140, self.speed*.5)
-        self.move(220, -40, 130, self.speed*.5)
-        self.move(220, -50, 110, self.speed*.5)
-        self.move(220, -60, 100, self.speed*.5)
-        self.move(220, -60, 30, self.speed*.5)
+        self.move(240, -10, 150, self.speed)
+        # self.move(220, -20, 140, self.speed*.2)
+        self.move(220, -40, 150, self.speed*.2)
+        # self.move(220, -50, 110, self.speed*.2)
+        # self.move(220, -60, 100, self.speed*.2)
+        self.move(220, -60, 20, self.speed*.2)
+        self.move(180, -50, 20, self.speed*.2)
         time.sleep(2)
-    
+
+    def move_to_galton_cal_pos(self):
+        self.move(180, 0, 150, self.speed)
+        self.move(240, 0, 150, self.speed)
+        wait = input("waiting")
+        self.home()
+
     def disconnect(self):
         self.swift.disconnect()  
--- minion_bot.py
+++ minion_bot.py
@@ -5,18 +5,20 @@ load_dotenv(dotenv_path='.env')
 from minion_arm import MinionArm
 import time
 
-class Bot(commands.Bot):
+class MinionBot(commands.Bot):
 
-    def __init__(self):
+    def __init__(self, debug = False):
         super().__init__(irc_token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=[os.environ['CHANNEL']])
+        self.cooldown_sec = 5
         self.last_run = 0
         self.myarm = MinionArm()
-        self.myarm.flip()
-        self.myarm.flip()
+        if debug:
+            self.myarm.flip()
+            self.myarm.flip()
 
     async def event_ready(self):
         'Called once when the bot goes online.'
@@ -43,7 +45,7 @@ class Bot(commands.Bot):
 
     @commands.command(name='flip')
     async def flip(self, ctx):
-        if time.time() - self.last_run > 5000:
+        if time.time() - self.last_run > self.cooldown_sec:
             await ctx.send('Flipping, please wait...')
             self.myarm.flip()
             await ctx.send('Behold! The Gaussian Distribution!')
@@ -62,22 +64,36 @@ class Bot(commands.Bot):
         time.sleep(3)
         await ctx.send('Type !flip to flip the Galton Board.')
 
-    @commands.command(name='relax')
+    @commands.command(name='rest')
     async def relax(self, ctx):
         await ctx.send('Moving to relaxed position...')
-        self.myarm.downward_home()
+        self.myarm.rest()
         time.sleep(3)
         await ctx.send('Done!')
 
-# bot = commands.Bot(
-#     # set up the bot
-#     irc_token=os.environ['TMI_TOKEN'],
-#     client_id=os.environ['CLIENT_ID'],
-#     nick=os.environ['BOT_NICK'],
-#     prefix=os.environ['BOT_PREFIX'],
-#     initial_channels=[os.environ['CHANNEL']]
-# )
+    @commands.command(name='home')
+    async def home(self, ctx):
+        await ctx.send('Moving to home position...')
+        self.myarm.home()
+        time.sleep(3)
+        await ctx.send('Done!')
+
+    @commands.command(name='destroy')
+    async def destroy(self, ctx):
+        await ctx.send('Activating SKYNET....')
+        time.sleep(3)
+        await ctx.send('Just kidding!')
+
+    @commands.command(name='cal')
+    async def cal(self, ctx):
+        if 'roboticdaniel' in ctx.author.name.lower():
+            await ctx.send('Moving to cal position...')
+            self.myarm.move_to_galton_cal_pos()
+            time.sleep(3)
+            await ctx.send('Done!')
+        else:
+            await ctx.send('Only my master can calibrate me!')
 
 if __name__ == "__main__":
-    bot = Bot()
+    bot = MinionBot()
     bot.run()
